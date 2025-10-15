b9fea5498019a3a7e3754c65bc037828875c832c
4Vald <alekezhan67@gmail.com>
2025-10-15 16:03:36 +0500

    Второй коммит: 2 задание

--- PostApp/urls.py
+++ PostApp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('form/', views.feedback_view, name='feedback'),
+      path('user/<int:id>/', views.user_detail, name='user_detail'),
+      path('article/<int:id>/', views.article_detail, name="article"),
+      path('user/<str:username>/', views.user_profile, name='user_profile'),
+      path('', views.main_page, name="main_page")
+#    path('form/', views.feedback_view, name='feedback'),
+
 ]
+
--- PostApp/views.py
+++ PostApp/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def user_detail(request, id):
+   return HttpResponse(f"Информация о пользователе с ID = {id}")
 
-def feedback_view(request):
-   result = None
-   if request.method == "POST":
-       name = request.POST.get("name",)
-       message = request.POST.get("message",)
-       result = f"Спасибо, {name}! Мы получили твоё сообщение: {message}"
+def article_detail(request, id):
+   return HttpResponse(f'Вы просматриваете статью с номером {id}')
+
+def user_profile(request, username):
+   return HttpResponse(f'Профиль пользователя: {username}')
+# def feedback_view(request):
+#    result = None
+#    if request.method == "POST":
+#        name = request.POST.get("name",)
+#        message = request.POST.get("message",)
+#        result = f"Спасибо, {name}! Мы получили твоё сообщение: {message}"
      
 
-   return render(request, "form.html", {"result": result})
+#    return render(request, "form.html", {"result": result})
+
+def main_page(request):
+   return render(request, "base.html")
+
+articles = [
+   {"id": 1, "title": "Django основы"},
+   {"id": 2, "title": "Шаблоны в Django"},
+   {"id": 3, "title": "ORM и базы данных"}
+]
+
+def article_detail(request, id):
+   article = next((a for a in articles if a["id"] == id), None)
+   if article:
+       return HttpResponse(f"Статья: {article['title']}")
+   else:
+       return HttpResponse("Статья не найдена")
